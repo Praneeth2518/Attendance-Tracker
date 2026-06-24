fb1d59e4475d2fdd4c23d6a8ad2d75e58a3818ac
Praneeth <praneethgh16@gmail.com>
2026-06-24 22:31:36 +0530

    add middleware

---
+++ middleware/error.middleware.js
@@ -0,0 +1,8 @@
+export const errorMiddleware = (err, req, res, next) => {
+    console.error(err);
+
+    res.status(err.statusCode || err.status || 500).json({
+        success: false,
+        message: err.message || 'Server error'
+    });
+}
