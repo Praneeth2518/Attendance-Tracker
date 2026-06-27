d99d932b53d8c2e278dead30daa5a0c578876be1
Praneeth <praneethgh16@gmail.com>
2026-06-27 22:42:01 +0530

    fix errors

--- config/env.js
+++ config/env.js
@@ -1,11 +1,18 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-// set development and production switch later
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+const result = dotenv.config({
+    path: `.env.${process.env.NODE_ENV || "development"}.local`,
+});
+
+// console.log(result);
+// console.log("Error:", result.error);
+// console.log("Parsed:", result.parsed);
+// console.log("DB_URI:", process.env.DB_URI);
 
 export const {
     PORT,
     DB_URI,
     ADMIN_KEY,
-    JWT_SECRET, JWT_EXPIRES_IN
+    JWT_SECRET,
+    JWT_EXPIRES_IN
 } = process.env;
--- middleware/error.middleware.js
+++
@@ -1,8 +0,0 @@
-export const errorMiddleware = (err, req, res, next) => {
-    console.error(err);
-
-    res.status(err.statusCode || err.status || 500).json({
-        success: false,
-        message: err.message || 'Server error'
-    });
-}
